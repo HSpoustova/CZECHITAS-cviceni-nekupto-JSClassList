975765fe5b64004552a4ad9232a9bd1ee465a4fb
Hana Spoustová <bernreih@gmail.com>
2023-12-18 22:23:02 +0100

    reseni

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const product1 = {
   image: 'images/destniklobouk.jpg',
   name: 'Deštníklobouk',
-  description: 'Vynikající pokud si často zapomínáte deštník a nechcete být nikdy překvapeni nečekanou přeháňkou.',
+  description:
+    'Vynikající pokud si často zapomínáte deštník a nechcete být nikdy překvapeni nečekanou přeháňkou.',
 };
 
 const product2 = {
   image: 'images/slepickabelka.jpg',
   name: 'Slepičkabelka',
-  description: 'Nejlepší způsob jak zaujmout neotřelou módou v ulíčkách kolem Staroměstkého náměstí.',
+  description:
+    'Nejlepší způsob jak zaujmout neotřelou módou v ulíčkách kolem Staroměstkého náměstí.',
 };
 
 const product3 = {
   image: 'images/anatoplavky.jpg',
   name: 'Anatoplavky',
-  description: 'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
+  description:
+    'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
+
+const product1Element = document.querySelector('#product1');
+product1Element.classList.add('border-primary');
+
+const product2Element = document.querySelector('#product2 button');
+product2Element.classList.remove('btn-primary');
+
+const product3Element = document.querySelector('#product3 .card-title');
+
+product3Element.classList.toggle('text-center');
